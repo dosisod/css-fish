f4fd8af7ef470b330957751ac1e9ff3dcb0b469d
dosisod <39638017+dosisod@users.noreply.github.com>
2018-12-29 23:19:55 -0800

    cleaned up variables

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # pass something like "123 4567 file.css" and a table named "file".css for sizes 0x0 to 123x4567 will be made
 
-name=$(awk '{print $3}' <<< $@)
+name=$3
 >"$name" # clears file
 
-for ((i=0;i<=$(awk '{print $1}' <<< $@);i++)); do # does all of the css selectors for width
+for ((i=0;i<=$1;i++)); do # makes css selectors for width
 	echo -n "@media (width:"$i"px){#w{background:url('/?w="$i"')}}" >> "$name"
 done
 
-for ((i=0;i<=$(awk '{print $2}' <<< $@);i++)); do # makes all css selectors for height
+for ((i=0;i<=$2;i++)); do # makes css selectors for height
 	echo -n "@media (height:"$i"px){#h{background:url('/?h="$i"')}}" >> "$name"
 done
