7ffb6c9c799f6ea446d434ea229235f6b2351bba
dosisod <39638017+dosisod@users.noreply.github.com>
2018-12-30 19:59:35 -0800

    updated php

--- index.php
+++ index.php
@@ -8,8 +8,10 @@ if (isset($_GET)) { //if parameter is passed, save it
 	if (isset($_GET["h"])) { $_SESSION["height"]=$_GET["h"]; }
 }
 if (isset($_SESSION["width"]) && isset($_SESSION["height"])) {
+	$_SESSION["done"]=true
+}
+if (isset($_SESSION["done"])) { //only use info if all data has been received
 	//do something with the screen size (track user etc)
-	session_destroy();
 }
 else { //else print the HTML to grab dimentions
 	echo "<link rel='stylesheet' href='".$file."'><img id='h'><img id='w'>";
